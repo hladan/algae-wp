0f33f42e38abb52ca2332b4a66b3a4c96189a9fa
hladan <hamed.ladan@gmail.com>
2015-05-10 18:28:11 +0430

    1- Changed background color of the footer and bottom footer. 2- Edited the copyright in the bottom footer.

--- wp-content/themes/road-fighter/footer.php
+++ wp-content/themes/road-fighter/footer.php
@@ -19,7 +19,9 @@
             <div class="bottom_footer_content">
                 <div class="grid_24">
                     <div class="copyrightinfo">
-                        <p class="copyright"><a href="<?php echo urldecode('http://www.inkthemes.com'); ?>"><?php _e('RoadFighter Theme','rdf'); ?></a> <?php _e('Powered By','rdf'); ?><a href="<?php echo urlencode('http://www.wordpress.org'); ?>"><?php _e('WordPress','rdf'); ?></a></p>
+                        <p class="copyright"><?php _e('Realized by','rdf'); ?><a href="<?php echo urldecode('http://www.wordpress.com'); ?>" target="_blank">
+                        	                 <?php _e('Wordpress','rdf'); ?></a> <?php _e('&copy; 2015 marinature.com','rdf'); ?></p>
+
                     </div>
                 </div>
             </div>
