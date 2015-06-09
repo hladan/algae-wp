a21b0d5ab7eb02c90d3870a4263966ffe4579a04
hladan <hamed.ladan@gmail.com>
2015-06-09 12:51:27 +0430

    test pushing

--- index.php
+++ index.php
@@ -10,13 +10,7 @@
  * Tells WordPress to load the WordPress theme and output it.
  *
  * @var bool
- */
- 
- 
- 
- 
- 
- 
+ */  
   define('WP_USE_THEMES', true);  
 /** Loads the WordPress Environment and Template */
 require( dirname( __FILE__ ) . '/wp-blog-header.php' );
